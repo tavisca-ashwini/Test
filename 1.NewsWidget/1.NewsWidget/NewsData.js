002b6b491d5b7f76c70e123a1ad30504be41cd78
unknown <akardile@AshwiniK.internal.tavisca.com>
2015-09-04 02:17:33 +0530

    commit with tabs-toggle

--- 1.NewsWidget/1.NewsWidget/NewsData.js
+++ 1.NewsWidget/1.NewsWidget/NewsData.js
@@ -1,16 +1,23 @@
 ﻿
 var wHTML = "", json = "";
-
+wHTML += ('<div id="myDiv">');
 wHTML += ('<div class="container">');
-wHTML += ('<ul class="nav nav-tabs"> <li class="active"><a href="#Delhi">delhi</a></li>');
-wHTML += ('<li><a href="#hyderabad">hyderabad</a></li><li><a href="#banglore">banglore</a></li> <li><a href="#chennai">chennai</a></li> <li><a href="#pune">pune</a></li> <li><a href="#mumbai">mumbai</a></li> <li><a href="#kolkata">kolkata</a></li>');
-wHTML += ('</ul></div></center>');
+wHTML += ('<ul class="nav nav-tabs" role="tablist"> <li class="active" role="presentation"><a href="#Delhi" role="tab" data-toggle="tab">Delhi</a></li>');
+wHTML += ('<li role="tablist"><a href="#hyderabad" role="tab" data-toggle="tab">hyderabad</a></li><li role="tablist"><a href="#banglore" role="tab" data-toggle="tab">banglore</a></li> <li role="tablist"><a href="#chennai" role="tab" data-toggle="tab">chennai</a></li>');
+wHTML += ('<li role="tablist"><a href="#pune" role="tab" data-toggle="tab">pune</a></li> <li role="tablist"><a href="#mumbai" role="tab" data-toggle="tab">mumbai</a></li> <li role="tablist"><a href="#kolkata" role="tab" data-toggle="tab">kolkata</a></li>');
+wHTML += ('</ul></div>');
+wHTML += ('<div role="tabpanel" class="tab-pane active" id="Delhi"> </div> <div role="tabpanel" class="tab-pane" id="hyderabad"> </div>');
+wHTML += ('<div role="tabpanel" class="tab-pane" id="banglore"> </div> <div role="tabpanel" class="tab-pane" id="chennai"> </div>');
+wHTML += ('<div role="tabpanel" class="tab-pane" id="pune"> </div> <div role="tabpanel" class="tab-pane" id="mumabi"> </div> <div role="tabpanel" class="tab-pane" id="kolkata"></div>');
+
+
 document.write(wHTML);
 
 $(document).ready(function () {
-    $(".nav-tabs a").click(function () {
-        
+    $(".nav-tabs a").click(function (e) {
+        //e.preventDefault();
         $(this).tab('show');
+
         $.getJSON("http://training.appyoda.io/api/news/" + this.innerText, function (data) {
             console.log(data);
             jQuery.each(data.responseData.feed.entries, function (i, entries)
