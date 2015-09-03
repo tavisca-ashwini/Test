adb040226309ac0e9371f7a64f6f9d7b7ec46264
unknown <akardile@AshwiniK.internal.tavisca.com>
2015-09-04 00:20:33 +0530

    commit with tabs

--- 1.NewsWidget/1.NewsWidget/NewsData.js
+++ 1.NewsWidget/1.NewsWidget/NewsData.js
@@ -1,14 +1,23 @@
 ﻿
-var wHTML = "";
+var wHTML = "", json = "";
+
 wHTML += ('<div class="container">');
-wHTML += ('<ul class="nav nav-tabs"> <li class="active"><a href="#home">Home</a></li>');
-wHTML += ('<li><a href="#Delhi">Delhi</a></li> <li><a href="#Mumbai">Mumbai</a></li> <li><a href="#Hyderabad">Hyderabad</a></li> <li><a href="#Bangalore">Bangalore</a></li> <li><a href="#Chennai">Chennai</a></li> <li><a href="#Pune">Pune</a></li> <li><a href="#Kolkata">Kolkata</a></li>');
+wHTML += ('<ul class="nav nav-tabs"> <li class="active"><a href="#Delhi">delhi</a></li>');
+wHTML += ('<li><a href="#hyderabad">hyderabad</a></li><li><a href="#banglore">banglore</a></li> <li><a href="#chennai">chennai</a></li> <li><a href="#pune">pune</a></li> <li><a href="#mumbai">mumbai</a></li> <li><a href="#kolkata">kolkata</a></li>');
 wHTML += ('</ul></div></center>');
+document.write(wHTML);
 
 $(document).ready(function () {
     $(".nav-tabs a").click(function () {
-        alert(this);
+        
         $(this).tab('show');
+        $.getJSON("http://training.appyoda.io/api/news/" + this.innerText, function (data) {
+            console.log(data);
+            jQuery.each(data.responseData.feed.entries, function (i, entries)
+            {
+                json += ('<div id=><a href=" ' + entries.link + '"" id ="#link">' + entries.title + '</a> </br> </div>')
+            });
+            document.write(json);
+        });
     });
 });
-document.write(wHTML);
