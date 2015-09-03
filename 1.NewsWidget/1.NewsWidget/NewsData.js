656d091ebf6044159dcc385a6fddee1ad287611a
unknown <akardile@AshwiniK.internal.tavisca.com>
2015-09-03 21:28:24 +0530

    first commit

---
+++ 1.NewsWidget/1.NewsWidget/NewsData.js
@@ -0,0 +1,14 @@
+﻿
+var wHTML = "";
+wHTML += ('<div class="container">');
+wHTML += ('<ul class="nav nav-tabs"> <li class="active"><a href="#home">Home</a></li>');
+wHTML += ('<li><a href="#Delhi">Delhi</a></li> <li><a href="#Mumbai">Mumbai</a></li> <li><a href="#Hyderabad">Hyderabad</a></li> <li><a href="#Bangalore">Bangalore</a></li> <li><a href="#Chennai">Chennai</a></li> <li><a href="#Pune">Pune</a></li> <li><a href="#Kolkata">Kolkata</a></li>');
+wHTML += ('</ul></div></center>');
+
+$(document).ready(function () {
+    $(".nav-tabs a").click(function () {
+        alert(this);
+        $(this).tab('show');
+    });
+});
+document.write(wHTML);
---
+++ 1.NewsWidget/1.NewsWidget/Widget.js
@@ -0,0 +1,10 @@
+﻿//wWidth = "260px";
+//wHeight = "195px";
+//wFColor = "#627ea6";
+//wTitle = "Hello";
+//wURL = "http://widgets-gadgets.com";
+//document.write('<div id="myFirstWidget"></div>');
+
+document.write('<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">')
+document.write('<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>')
+document.write('<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>')
