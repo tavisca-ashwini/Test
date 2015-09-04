e26ad577b63c1a4da57ee6153c154d43eb603eef
unknown <akardile@AshwiniK.internal.tavisca.com>
2015-09-04 16:58:53 +0530

    Code for NewsWidget

--- 1.NewsWidget/1.NewsWidget/NewsData.js
+++ 1.NewsWidget/1.NewsWidget/NewsData.js
@@ -1,12 +1,16 @@
 ﻿
+document.write('<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">')
+document.write('<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>')
+document.write('<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>')
+
 var wHTML = "", json = "";
 wHTML += ('<div id="myDiv">');
 wHTML += ('<div class="container">');
-wHTML += ('<ul class="nav nav-tabs" role="tablist"> <li class="active" role="presentation"><a href="#Delhi" role="tab" data-toggle="tab">Delhi</a></li>');
+wHTML += ('<ul class="nav nav-tabs" role="tablist"> <li class="active" role="presentation"><a href="#delhi" role="tab" data-toggle="tab">Delhi</a></li>');
 wHTML += ('<li role="tablist"><a href="#hyderabad" role="tab" data-toggle="tab">hyderabad</a></li><li role="tablist"><a href="#banglore" role="tab" data-toggle="tab">banglore</a></li> <li role="tablist"><a href="#chennai" role="tab" data-toggle="tab">chennai</a></li>');
 wHTML += ('<li role="tablist"><a href="#pune" role="tab" data-toggle="tab">pune</a></li> <li role="tablist"><a href="#mumbai" role="tab" data-toggle="tab">mumbai</a></li> <li role="tablist"><a href="#kolkata" role="tab" data-toggle="tab">kolkata</a></li>');
 wHTML += ('</ul></div>');
-wHTML += ('<div role="tabpanel" class="tab-pane active" id="Delhi"> </div> <div role="tabpanel" class="tab-pane" id="hyderabad"> </div>');
+wHTML += ('<div role="tabpanel" class="tab-pane active" id="delhi"> </div> <div role="tabpanel" class="tab-pane" id="hyderabad"> </div>');
 wHTML += ('<div role="tabpanel" class="tab-pane" id="banglore"> </div> <div role="tabpanel" class="tab-pane" id="chennai"> </div>');
 wHTML += ('<div role="tabpanel" class="tab-pane" id="pune"> </div> <div role="tabpanel" class="tab-pane" id="mumabi"> </div> <div role="tabpanel" class="tab-pane" id="kolkata"></div>');
 
